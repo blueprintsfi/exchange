6907caae486b8bb7c255ff4a75aa985cdd0e68af
Czar102 <lo2.galinskic@gmail.com>
2025-02-09 14:17:21 +0100

    fix whitespace chars

--- test/fex.t.sol
+++ test/fex.t.sol
@@ -27,7 +27,7 @@ contract fExchangeSimplifiedTest is Test {
     address public user;
     address public operator;
     uint256 public constant delay = 1 days;
-    
+
     function setUp() public {
         // Deploy BlueprintManager and TestableFExchange.
         blueprintManager = new BlueprintManager();
@@ -36,7 +36,7 @@ contract fExchangeSimplifiedTest is Test {
         operator = makeAddr("operator");
         vm.deal(user, 100 ether);
         vm.deal(operator, 100 ether);
-        
+
         // Set up the blueprint manager for the user.
         vm.startPrank(user);
         blueprintManager.setOperator(address(blueprintManager), true);
@@ -46,7 +46,7 @@ contract fExchangeSimplifiedTest is Test {
         blueprintManager.setOperator(address(exchange), true);
         vm.stopPrank();
     }
-    
+
     /// @dev Helper to execute a deposit action.
     /// Since executeAction is protected by onlyManager, we simulate a call from the blueprint manager.
     function executeDeposit(TokenOp[] memory deposits) internal {
@@ -54,28 +54,28 @@ contract fExchangeSimplifiedTest is Test {
         vm.prank(address(blueprintManager));
         exchange.executeAction(action);
     }
-    
+
     /// @notice Test that multiple deposits accumulate correctly.
     function test_MultipleDepositsAccumulation() public {
         // First deposit: 50 tokens.
         TokenOp[] memory deposits1 = new TokenOp[](1);
         deposits1[0] = TokenOp(1, 50);
         executeDeposit(deposits1);
-        
+
         // Second deposit: 25 tokens.
         TokenOp[] memory deposits2 = new TokenOp[](1);
         deposits2[0] = TokenOp(1, 25);
         executeDeposit(deposits2);
-        
+
         uint256 bal = exchange.getBalance(user, 1, operator, delay);
         assertEq(bal, 75, "Accumulated deposit should be 75");
     }
-    
+
     /// @notice Test that deposits for different operators are stored separately.
     function test_DepositDifferentOperators() public {
         TokenOp[] memory deposits = new TokenOp[](1);
         deposits[0] = TokenOp(1, 100);
-        
+
         // Deposit for operatorA.
         address operatorA = makeAddr("operatorA");
         {
@@ -90,13 +90,13 @@ contract fExchangeSimplifiedTest is Test {
             vm.prank(address(blueprintManager));
             exchange.executeAction(actionB);
         }
-        
+
         uint256 balA = exchange.getBalance(user, 1, operatorA, delay);
         uint256 balB = exchange.getBalance(user, 1, operatorB, delay);
         assertEq(balA, 100, "OperatorA balance should be 100");
         assertEq(balB, 100, "OperatorB balance should be 100");
     }
-    
+
     /// @notice Test that calling deposit with an empty array does not update the balance.
     function test_DepositEmptyArray() public {
         TokenOp[] memory deposits = new TokenOp[](0);
@@ -107,7 +107,7 @@ contract fExchangeSimplifiedTest is Test {
         uint256 bal = exchange.getBalance(user, 1, operator, delay);
         assertEq(bal, 0, "Empty deposit should leave balance unchanged");
     }
-    
+
     /// @notice Test that depositing a zero amount does not update the balance.
     function test_DepositZeroAmount() public {
         TokenOp[] memory deposits = new TokenOp[](1);
@@ -118,37 +118,37 @@ contract fExchangeSimplifiedTest is Test {
         uint256 bal = exchange.getBalance(user, 1, operator, delay);
         assertEq(bal, 0, "Deposit of zero amount should leave balance unchanged");
     }
-    
+
     /// @notice Test depositing a large (but moderate) amount.
     function test_DepositMaxAmount() public {
-        uint256 depositAmount = 10000; 
+        uint256 depositAmount = 10000;
         TokenOp[] memory deposits = new TokenOp[](1);
         deposits[0] = TokenOp(1, depositAmount);
         executeDeposit(deposits);
         uint256 bal = exchange.getBalance(user, 1, operator, delay);
         assertEq(bal, depositAmount, "Max deposit not recorded correctly");
     }
-    
+
     /// @notice Test ragequit by the deposit holder and then unragequit.
     function test_RagequitAndUnragequit() public {
         // Deposit tokens.
         TokenOp[] memory deposits = new TokenOp[](1);
         deposits[0] = TokenOp(1, 200);
         executeDeposit(deposits);
-        
+
         // Ragequit by the user.
         vm.prank(user);
         exchange.ragequit(operator, delay);
         (, uint256 rqTime) = exchange.userData(user, operator, delay);
         assertTrue(rqTime != 0, "Ragequit time should be set");
-        
+
         // Unragequit by the user.
         vm.prank(user);
         exchange.unragequit(operator, delay);
         (, uint256 rqTimeAfter) = exchange.userData(user, operator, delay);
         assertEq(rqTimeAfter, 0, "Ragequit time should be cleared");
     }
-    
+
     /// @notice Test that a call to ragequit from an address other than the deposit holder
     /// sets the ragequit time for that caller (since no access control is implemented).
     function test_RagequitUnauthorized() public {
@@ -156,7 +156,7 @@ contract fExchangeSimplifiedTest is Test {
         TokenOp[] memory deposits = new TokenOp[](1);
         deposits[0] = TokenOp(1, 100);
         executeDeposit(deposits);
-        
+
         // Now simulate an "unauthorized" ragequit by the operator.
         // In the current implementation, this call does NOT revert.
         vm.prank(operator);
